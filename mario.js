2a16eaed456c07005cbac2c8c219b1b1585efadb
hosam3574 <hossamtradat@gmail.com>
2025-04-21 00:25:53 +0300

    mario

---
+++ mario.js
@@ -0,0 +1,22 @@
+var mario=document.getElementById('mario')
+
+
+
+function moveUp(){
+console.log("up")
+
+}
+function moveDown(){
+    console.log("down")
+    
+    }
+
+    function moveLift(){
+        console.log("lift")
+        
+        }
+
+        function moveRight(){
+            console.log("right")
+            
+            }
